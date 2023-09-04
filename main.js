d979954b78adfa802f2a74b0edb87c67cf6e8f79
hosam mohamed <hosmoh324@gmail.com>
2023-09-04 22:47:44 +0300

    fourth js challenge

---
+++ main.js
@@ -0,0 +1,6 @@
+let shareButton = document.querySelector(".share_image");
+let shareBox = document.querySelector(".share_box");
+
+shareButton.addEventListener("click", () => {
+  shareBox.style.display = "flex";
+});
